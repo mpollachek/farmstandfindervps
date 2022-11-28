fecf04b8514c84bae37af286c40ca6578b5a2c0a
mpollachek <mpollachek81@gmail.com>
2022-11-28 10:56:53 -0500

    functional starting point from last project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Outlet } from "react-router-dom";
+import FarmstandsPage from "./pages/FarmstandsPage";
+import FarmstandDetailPage from "./pages/FarmstandDetailPage";
+import MapsPage from "./pages/MapsPage";
+import TestPage from "./pages/TestPage";
 
-function App() {
+const App = () => {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<MapsPage />} />
+        <Route path="farmstands" element={<FarmstandsPage />} />
+          <Route
+            path="farmstands/:farmstandId"
+            element={<FarmstandDetailPage />}
+          />
+        <Route path="test" element={<TestPage />} />
+      </Routes>
     </div>
   );
 }
