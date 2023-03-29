b60a74b417872834d38a6870eb3008f7b72d812a
mpollachek <mpollachek81@gmail.com>
2023-03-28 21:38:45 -0400

    validation corrections

--- src/forms/validations.js
+++ src/forms/validations.js
@@ -1,19 +1,19 @@
 import * as yup from 'yup'
 
 export const loginSchema = yup.object({
-    username: yup.string().min(3, "must be at least 3 characters").max(20, "must be 20 characters or less").required("required"),
+    username: yup.string().min(3, "must be at least 3 characters").max(40, "must be 40 characters or less").required("required"),
     password: yup.string().min(6, "Minimum 6 characters with 1 uppercase letter, 1 lowercase letter and 1 number").lowercase(1, "Minimum 6 characters with 1 uppercase letter, 1 lowercase letter and 1 number").uppercase("Minimum 6 characters with 1 uppercase letter, 1 lowercase letter and 1 number").required('Required')
 });
 
 export const registerSchema = yup.object({
-  registerusername: yup.string().min(3, "must be at least 3 characters").max(20, "must be 20 characters or less").required("required"),
+  registerusername: yup.string().min(3, "must be at least 3 characters").max(30, "must be 30 characters or less").required("required"),
   registerpassword: yup.string().min(6, "Minimum 6 characters with 1 uppercase letter, 1 lowercase letter and 1 number").lowercase(1, "Minimum 6 characters with 1 uppercase letter, 1 lowercase letter and 1 number").uppercase("Minimum 6 characters with 1 uppercase letter, 1 lowercase letter and 1 number").required('required'),
   confirmpassword: yup.string().required("required"),
   useremail: yup.string().email("Please enter a valid email").required("required")
 });
 
 export const createListingSchema = yup.object({
-  farmstandName: yup.string().min(3, "must be at least 3 characters").max(30, "must be 30 characters or less").required("required"),
+  farmstandName: yup.string().min(3, "must be at least 3 characters").max(100, "must be 100 characters or less").required("required"),
 });
 
 export const addImagesSchema = yup.object({
@@ -28,7 +28,7 @@ export const profileSchema = yup.object({
 });
 
 export const profileUsernameSchema = yup.object({
-  newUsername: yup.string().min(3, "must be at least 3 characters").max(20, "must be 20 characters or less").required("Required")
+  newUsername: yup.string().min(3, "must be at least 3 characters").max(30, "must be 30 characters or less").required("Required")
 });
 
 export const profilePasswordSchema = yup.object({
